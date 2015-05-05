faf57a68d011c7dc422011690b83b1b92f71fcaf
Manuel VIVES <manozco@gmail.com>
2015-05-05 15:19:13 -0400

    Add merge_sort

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -3,7 +3,7 @@
 # @Author: Manuel VIVES
 # @Date:   2015-05-04 14:22:54
 # @Last Modified by:   Manuel VIVES
-# @Last Modified time: 2015-05-05 12:44:04
+# @Last Modified time: 2015-05-05 15:19:03
 
 
 def insertion_sort(l: list):
@@ -27,3 +27,31 @@ def bubble_sort(l: list):
             break
 
 
+def merge_sort(a_list: list):
+    if len(a_list) > 1:
+        mid = len(a_list) // 2
+        lefthalf = a_list[:mid]
+        righthalf = a_list[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+
+        i, j, k = 0, 0, 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                a_list[k] = lefthalf[i]
+                i = i + 1
+            else:
+                a_list[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            a_list[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            a_list[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
